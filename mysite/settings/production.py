ee941e28a5e57b5232263e36da2721bc9d35bc0c
yash soni <yashsoni99777@gmail.com>
2019-03-21 16:06:00 +0530

    for settings folder and settings folder

---
+++ mysite/settings/production.py
@@ -0,0 +1,39 @@
+from .base import *
+import os
+#from boto.s3.connection import S3Connection
+
+#SECRET_KEY = S3Connection(os.environ['SECRET_KEY'])
+SECRET_KEY = os.environ['SECRET_KEY']
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['dearproblems.herokuapp.com']
+
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+MY = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+STATIC_ROOT  =   os.path.join(MY, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra lookup directories for collectstatic to find static files
+STATICFILES_DIRS = (
+    os.path.join(MY, 'static'),
+)
+
+
+
+import dj_database_url
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -7,10 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
+
+
+
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings.production')
 
 application = get_wsgi_application()
